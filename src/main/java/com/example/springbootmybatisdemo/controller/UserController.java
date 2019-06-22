df30b3b1208475ef3daad3c8fcc94bc5236d8cc4
枫叶 <44988649+wangzj5@users.noreply.github.com>
2019-06-22 19:15:11 +0800

    第2提交spring boot整合mybatis,创建了一个线程安全的Map

--- src/main/java/com/example/springbootmybatisdemo/controller/UserController.java
+++ src/main/java/com/example/springbootmybatisdemo/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.example.springbootmybatisdemo.controller;
 
+import com.example.springbootmybatisdemo.bean.User;
 import com.example.springbootmybatisdemo.service.UserService;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/testBoot")
@@ -12,9 +18,11 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
+    //创建线程安全的hashmap
+    static Map<Long, User> userMap = Collections.synchronizedMap(new HashMap<Long,User>());
     @RequestMapping("getUser/{id}")
     public String GetUser(@PathVariable Long id){
+
         return userService.Sel(id).toString();
     }
 }
